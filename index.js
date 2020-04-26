d260140da64b79083db9835ee29a23b907543805
epic-store-5589 <s.midda1@gmail.com>
2020-04-26 07:55:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// write your code below!
 function happyHolidays() {
-  return "happyHolidays"
-}
-
-function happHolidaysTo (Guji) {
-  return `Happy Holidays, ${name}!`
-}
-
-function happyCustomHolidayTo (holiday, name){
-  return `Happy ${holiday}, ${name}!'
-}
-
-function holidayCountdown (holiday, name) {
-  return `It's ${days} days until ${holiday}!`
+  return "Happy holidays!"
 }
